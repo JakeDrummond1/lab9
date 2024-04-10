3cf27d68d772498b510930786a309b448a75c990
bryanzzz0 <bryan.zhing@gmail.com>
2024-04-10 13:22:07 -0400

    added decode

--- lab9.py
+++ lab9.py
@@ -1,10 +1,18 @@
-#Jake Drummond
 def encode(password):
     encoded_password = ""
     for digit in password:
         encoded_digit = str((int(digit) + 3) % 10)
         encoded_password += encoded_digit
     return encoded_password
+
+
+def decode(password):
+    decoded_password = ''
+    for digit in password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
 
 def main():
     while True:
@@ -22,14 +30,12 @@ def main():
             print("Your password has been encoded and stored!\n")
 
         elif option == "2":
-            print("Decode function will be implemented later.\n")
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}\n")
 
         elif option == "3":
-            print("Exiting the program. Goodbye!")
             break
 
-        else:
-            print("Invalid option. Please try again.\n")
 
 if __name__ == "__main__":
     main()
